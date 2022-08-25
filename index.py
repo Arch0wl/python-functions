c4734ec22f54ba697d9baef70b777c857ea79a17
Arch0wl <annstarovoitov@gmail.com>
2022-08-25 13:01:38 -0400

    changes

--- index.py
+++ index.py
@@ -1,2 +1,13 @@
-print("Hello Function")
+# print("Hello Function")
+# print statement, lets create grreting function (defined)
 
+#def greeting():
+    # print("Hello Friend!")
+#greeting()
+
+def greeting(name,age="4"):
+    print("Hello " + name + ",you are " + age + "!")
+    print(f"Hello {name}, you are {age}!")
+    
+greeting("Brian","32")
+greeting("Olik")
